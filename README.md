cc4d0c9fa41f2438b4a7e05fbc845d85f199c05e
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-05-22 16:34:07 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # SVG_Logo_Maker
+
+## Description
+
+проект по генерации ридми файла
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+npm install
+
+## Usage
+
+npm start
+
+    - Enter up to three characters: qwe
+    - Enter the text color (color keyword or hexadecimal number)
+    - Choose a shape: "circle", "triangle", "square"
+    - Enter the shape color (color keyword or hexadecimal number)
+
+## Contributing
+
+fork and pull request
+
+## Tests
+
+npm test
+
+## License
+
+none
+
+## Questions
+
+For any questions, please reach out to me via [GitHub](https://github.com/VladislavApelgants) or [email](mailto:qwe@mail.com).
---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = require("inquirer");
+
+const generateSVG = require("./lib/generateSVG");
+
+const questions = [
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to three characters:",
+    validate: function (value) {
+      if (value.length > 3) {
+        return "Please enter up to three characters.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter the text color (color keyword or hexadecimal number):",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Choose a shape:",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter the shape color (color keyword or hexadecimal number):",
+  },
+];
+
+const generateSV = async (questions) => {
+  try {
+    const answers = await inquirer.prompt(questions);
+    generateSVG(answers);
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
+};
+
+generateSV(questions);
